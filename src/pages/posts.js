2ff5e4a84d4c52d768637076cb97addd34597b9c
Necdet Efe <quiloos39@gmail.com>
2021-05-28 02:05:58 +0300

    Fixed and improved looks of posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -7,33 +7,37 @@ import Layout from "../components/layout";
 let posts = [
   {
     id: 1,
-    title: "Looking for CNG 351 partner",
-    author: "Necdet Efe",
+    title: "Looking for CNG 352 partner",
+    author: {
+      id: 1,
+      name: "Mustafa",
+      surname: "Aygun",
+    },
     desc: "I am looking for partner that will motivate me.",
-  },
-  {
-    id: 2,
-    title: "Looking for CNG 351 partner",
-    author: "Necdet Efe",
-    desc: "I am looking for partner that will motivate me.",
-  },
-  {
-    id: 3,
-    title: "Looking for CNG 351 partner",
-    author: "Necdet Efe",
-    desc: "I am looking for partner that will motivate me.",
-  },
-  {
-    id: 4,
-    title: "Looking for CNG 351 partner",
-    author: "Necdet Efe",
-    desc: "I am looking for partner that will motivate me.",
-  },
-  {
-    id: 5,
-    title: "Looking for CNG 351 partner",
-    author: "Necdet Efe",
-    desc: "I am looking for partner that will motivate me.",
+    teamMembers: [
+      {
+        id: 1,
+        name: "Mustafa",
+        surname: "Aygun",
+        university: "METU",
+        year: "3",
+      },
+      {
+        id: 2,
+        name: "Necdet",
+        surname: "Efe",
+        university: "METU",
+        year: "3",
+      },
+    ],
+    comments: [
+      {
+        name: "Necdet",
+        surname: "Efe",
+        comment: "Amazing team mate had fun working with him.",
+        rating: 4,
+      },
+    ],
   },
 ];
 
@@ -43,7 +47,7 @@ const PostEntry = ({ post, setSelectedPost, ...props }) => {
       <div className="card-body">
         <Link
           className="card-title mb-2 d-block"
-          to={`/posts?id=${post.post_id}`}
+          to={`/posts?id=${post.id}`}
           style={{ textDecoration: "none" }}
           onClick={() => setSelectedPost(post)}
         >
@@ -51,10 +55,10 @@ const PostEntry = ({ post, setSelectedPost, ...props }) => {
         </Link>
         <Link
           className="card-subtitle mb-2 text-muted d-block"
-          to={`/users?id=${post.author_id}`}
+          to={`/users?id=${post.author.id}`}
           style={{ textDecoration: "none" }}
         >
-          {post.author}
+          {`${post.author.name} ${post.author.surname}`}
         </Link>
         <p className="card-text">{post.desc}</p>
       </div>
@@ -65,7 +69,6 @@ const PostEntry = ({ post, setSelectedPost, ...props }) => {
 PostEntry.propTypes = {
   post: PropTypes.object,
   setSelectedPost: PropTypes.func,
-  props: PropTypes.node,
 };
 
 const Navigator = () => {
@@ -102,20 +105,95 @@ const Navigator = () => {
   );
 };
 
+const Comment = ({ comment, ...props }) => {
+  return (
+    <div className="border mb-3 p-3">
+      <h5>{`${comment.name} ${comment.surname}`}</h5>
+      <hr />
+      <p>{comment.comment}</p>
+    </div>
+  );
+};
+
+Comment.propTypes = {
+  comment: PropTypes.object,
+};
+
+const TeamBoard = ({ teamMembers, ...props }) => {
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <td>Full name</td>
+            <td>School</td>
+            <td>Year</td>
+          </tr>
+        </thead>
+        <tbody>
+          {teamMembers.map((member, index) => (
+            <tr key={index}>
+              <td>
+                <Link
+                  to={`/users?id=${member.id}`}
+                  style={{ textDecoration: "none" }}
+                >{`${member.name} ${member.surname}`}</Link>
+              </td>
+              <td>{member.university}</td>
+              <td>{member.year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+TeamBoard.propTypes = {
+  teamMembers: PropTypes.arrayOf(PropTypes.object),
+};
+
 const Post = ({ post, setSelectedPost, ...props }) => {
   return (
-    <div className="border p-3" {...props}>
-      <h2>{post.title}</h2>
-      <Link to="/users">{post.author}</Link>
-      <hr />
-      <p>{post.desc}</p>
-      <button
-        className="btn btn-success"
-        onClick={() => setSelectedPost(null)}
-        style={{ width: "120px" }}
-      >
-        Back
-      </button>
+    <div className="border p-4" {...props}>
+      <div className="row">
+        <div className="col-lg-7">
+          <div className="mb-3">
+            <h2>{post.title}</h2>
+            <Link
+              to={`/users?id=${post.author.id}`}
+              style={{ textDecoration: "none" }}
+            >{`${post.author.name} ${post.author.surname}`}</Link>
+            <hr />
+            <p>{post.desc}</p>
+          </div>
+          <div className="mb-3">
+            <h3>Comments</h3>
+            <hr />
+            {post.comments.map((comment, index) => (
+              <Comment key={index} comment={comment} />
+            ))}
+          </div>
+
+          <div className="mb-3">
+            <button className="btn btn-success me-2" style={{ width: "120px" }}>
+              Join
+            </button>
+            <button
+              className="btn btn-danger me-2"
+              onClick={() => setSelectedPost(null)}
+              style={{ width: "120px" }}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-5">
+          <h2>Team mates</h2>
+          <hr />
+          <TeamBoard teamMembers={post.teamMembers} />
+        </div>
+      </div>
     </div>
   );
 };
