17c1f47404e2e4016f856b74880b01073b8a2fad
Necdet Efe <quiloos39@gmail.com>
2021-05-27 11:27:07 +0300

    Changed pickle rick image

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import headerImage from "../images/alex-kulikov-BrunIOLQMfQ-unsplash.jpg";
-import avatar from "../images/472-4723269_transparent-pikachu-face-clip-art-pikachu-chibi-transparent.png";
+import avatar from "../images/kisspng-rick-sanchez-morty-smith-pickle-rick-youtube-rick-rick-and-morty-5acb1188dd0a87.2585329315232577369054.png";
 
 const ContactPage = () => {
   return (
@@ -12,7 +12,7 @@ const ContactPage = () => {
       />
       <div className="container my-5">
         <div className="row">
-          <div className="col-lg-8">
+          <div className="col-lg-9">
             <div>
               <h2>Contact Us</h2>
               <p>
@@ -23,7 +23,7 @@ const ContactPage = () => {
               </p>
             </div>
           </div>
-          <div className="col-lg-4">
+          <div className="col-lg-3">
             <img src={avatar} style={{ width: "100%", height: "350px" }} />
           </div>
         </div>
