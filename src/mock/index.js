8385835ff6a463f9d33e1fbbde6d13a08fddb8dc
Necdet Efe <quiloos39@gmail.com>
2021-06-12 02:21:24 +0300

    Added delay to mock.

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
-let mock = new MockAdapter(axios);
+let mock = new MockAdapter(axios, { delayResponse: 500 });
 
 export default mock;
